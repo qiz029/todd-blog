3da9ede1c5e435f511109f6ab0bad9d7a1a583be
todd zheng <toddzheng@todds-mac-mini.local>
2026-05-14 17:21:39 -0700

    Rebrand header: ToddTalk (TED-inspired logo)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Todd Zheng";
+export const SITE_TITLE = "ToddTalk";
 export const SITE_DESCRIPTION = "Senior software engineer and tech lead. Writing about AI engineering, release platforms, developer tooling, and the messy parts of building software at scale.";
